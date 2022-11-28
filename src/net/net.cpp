b8c6742d96c2277ed0b3513a9b5fb1f7f241e506
chaowei <harry2300042@gmail.com>
2022-11-28 00:33:21 -0800

    integrate library net

---
+++ src/net/net.cpp
@@ -0,0 +1,16 @@
+#include "net.h"
+
+bool node_set_loopback_address(node_t* node, char *ip_addr){
+
+    return false;
+}
+
+bool node_set_intf_address(node_t* node, char* local_if, char* ip_addr, char mask){
+    
+    return false;
+}
+
+bool node_unset_intf_ip_address(node_t* node, char* local_if){
+
+    return false;
+}
