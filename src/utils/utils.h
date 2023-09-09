ef7cb44bda0c49a4ca25e6b21fed9886841acfe7
harry <harry2300042@gmail.com>
2023-09-10 00:25:46 +0800

    Implementation for q3

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define IS_MAC_BROADCAST_ADDR(mac) ( strlen(mac)==6 && strcmp(mac, "\xFF\xFF\xFF\xFF\xFF\xFF")==0 )
+
 void apply_mask(char* prefix, char mask, char* str_prefix);
 
 bool validate_prefix(const char* input);
